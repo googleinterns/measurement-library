07656c59986af9baf8255eb762f376a98f1a1c71
KJ Galvan <kaityagalvan@gmail.com>
2020-06-08 15:27:44 -0700

    renamed checkout and product screens

--- demo/src/screens/CheckoutScreen/CheckoutScreen.js
+++ demo/src/screens/CheckoutScreen/CheckoutScreen.js
@@ -4,10 +4,10 @@ import React from 'react';
  * Component for the shopping cart page.
  * @return {!JSX} Returns the shopping cart page.
  */
-export function CredentialScreen() {
+export function CheckoutScreen() {
   return (
     <div>
-      <h1>{`I'm the Credential Page!`}</h1>
+      <h1>{`I'm the Checkout Page!`}</h1>
     </div>
   );
 }
--- demo/src/screens/ProductScreen/ProductScreen.js
+++ demo/src/screens/ProductScreen/ProductScreen.js
@@ -4,10 +4,10 @@ import React from 'react';
  * Component to display a single item
  * @return {!JSX} Returns a component to view an item.
  */
-export function ViewItemScreen() {
+export function ProductScreen() {
   return (
     <div>
-      <h1>{`I'm the View item  Page!`}</h1>
+      <h1>{`I'm the Product Page!`}</h1>
     </div>
   );
 }
