e242cbdd1215f987205c58e67a0a855f2c8f980e
KJ Galvan <kaityagalvan@gmail.com>
2020-06-08 13:30:54 -0700

    added linter, fixed boilerplate code

--- demo/src/App.js
+++ demo/src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * Component to wrap the rest of the website with common components.
+ * @return {!JSX} Returns site presentation for header, navigation,
+ *     and shopping cart.
+ */
 function App() {
   return (
     <div className="App">
