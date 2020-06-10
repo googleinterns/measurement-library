ca711841bf521b6bf64c3598af61efff5583ddad
Alex Li <7alex7li@gmail.com>
2020-06-10 12:56:36 -0400

    Create form component of checkout screen

---
+++ demo/src/components/UserInfoForm/UserInfoForm.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import {Form, Col} from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+/*
+ * A template command to generate a basic text input form group.
+ * @param {string} label The label to display above the form.
+ * @param {!Form.Control} control The control element of the form.
+ * @param {boolean} [wholeCol=true] If this element is displayed on a
+ *      column by itself.
+ * @returns {!JSX} A form group with the given label and control.
+ */
+const makeFormGroup = (label, control, wholeCol=true) => {
+  return (
+    <Form.Group as={wholeCol ? 'div' : Col}>
+      <Form.Label>{label}</Form.Label>
+      {control}
+    </Form.Group>);
+};
+
+const /** !JSX */ userInformation = (
+  <Form.Row>
+    {makeFormGroup('First Name',
+        <Form.Control required
+          type="text"
+          defaultValue="Edgar"
+          placeholder="First Name (Required)"
+        />, false)}
+    {makeFormGroup('Last Name',
+        <Form.Control required
+          type="text"
+          placeholder="Last Name (Required)"
+        />, false)}
+  </Form.Row>
+);
+const /** !JSX */ companyName = makeFormGroup('Company Name',
+    <Form.Control
+      type="text"
+      placeholder="Company name (optional)"
+    />);
+const /** !JSX */ addressLine1 = makeFormGroup('Street Address Line 1',
+    <Form.Control required
+      type="text"
+      defaultValue="532 N. 7th Street"
+      placeholder="Address Line 1 (required)"
+    />);
+const /** !JSX */ addressLine2 = makeFormGroup('Street Address Line 2',
+    <Form.Control
+      type="text"
+      placeholder="Address Line 2 (optional)"
+    />);
+const /** !JSX */ regionalInfo = (
+  <Form.Row>
+    {makeFormGroup('City',
+        <Form.Control
+          type="text"
+          defaultValue="Philadelphia"
+          placeholder="City (optional)"
+
+        />, false)}
+    {makeFormGroup('State',
+        <Form.Control
+          type="text"
+          defaultValue="PA"
+          placeholder="State (optional)"
+        />, false)}
+  </Form.Row>
+);
+const /** !JSX */ countryAndZip = <Form.Row>
+  {makeFormGroup('Country', <Form.Control required
+    type="text"
+    defaultValue="USA"
+    placeholder="Country (required)"
+  />, false)}
+  {makeFormGroup('Zip Code',
+      <Form.Control
+        type="number"
+        defaultValue="19123"
+        placeholder="Zip (optional)"
+      />, false)}
+</Form.Row>;
+const /** !JSX */ email = makeFormGroup('Email',
+    <Form.Control
+      type = "email"
+      defaultValue="PoeTheCatLover@google.com"
+      placeholder="Email (optional)"
+    />);
+
+/**
+ * Creates a page component with a form to get user information.
+ * @return {!JSX} The component.
+ */
+export function UserInfoForm({formId}) {
+  return (
+    <Form id={formId}>
+      {userInformation}
+      {companyName}
+      {addressLine1}
+      {addressLine2}
+      {regionalInfo}
+      {countryAndZip}
+      {email}
+    </Form>
+  );
+}
+
+UserInfoForm.propTypes = {
+  formId: PropTypes.string,
+};
--- demo/src/screens/CheckoutScreen/CheckoutScreen.js
+++ demo/src/screens/CheckoutScreen/CheckoutScreen.js
@@ -1,13 +1,52 @@
 import React from 'react';
+import {Container, Col, Row, Button} from 'react-bootstrap';
+import {UserInfoForm} from '../../components/UserInfoForm/UserInfoForm.js';
+import {useHistory} from 'react-router-dom';
+import './CheckoutScreen.css';
 
 /**
- * @return {!JSX} Page component for where a user can purchase
- *     the items they've selected.
+ * The ID for the form the user will fill out on this page.
+ * @const
+ * @type {string}
+ */
+const FORM_ID = 'user-info-form';
+
+/**
+ * Page component for a user to enter in personal billing information
+ * and confirm the items in their cart.
+ * @return {!JSX} The component
  */
 export function CheckoutScreen() {
+  const /** !Object */ history = useHistory();
+
+  /**
+   * Navigate to the thank you page iff the user info form is valid.
+   * Otherwise, alert the user that the form is invalid.
+   */
+  function navIfFormValid() {
+    const form = document.getElementById(FORM_ID);
+    if (form.checkValidity()) {
+      // navigate to thank you page with react-router
+      history.push('/thanks');
+    } else {
+      form.reportValidity();
+    }
+  }
+
   return (
-    <div>
-      <h1>{`I'm the Checkout Page!`}</h1>
-    </div>
+    <Container>
+      <Row className="checkout-header">
+        <Col>Billing Details</Col>
+        <Col>Your order</Col>
+      </Row>
+      <Row className="checkout-content">
+        <Col>
+          <UserInfoForm formId={FORM_ID}/>
+        </Col>
+        <Col>
+          <Button onClick={navIfFormValid}>Submit Order</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
