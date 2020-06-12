2f9c8e8b247e777e0c1a00b135d3ac76d186c63a
Alex Li <7alex7li@gmail.com>
2020-06-12 14:24:23 -0400

    Add Code Modal component to highlight and display analytics code

---
+++ demo/src/components/CodeModal/CodeModal.js
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react';
+import {Button, Modal, Tabs, Tab} from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import './CodeModal.css';
+import Prism from 'prismjs';
+import '../../vendor/prism.css';
+
+CodeModal.propTypes = {
+  popupId: PropTypes.string,
+  ourCode: PropTypes.string,
+  gtagCode: PropTypes.string,
+};
+/**
+ * A popup to display the google analytics code that fires on
+ * a button click.
+ * @param {string} popupId The ID of this popup.
+ * @param {string} ourCode The code for this open source library
+ * @param {string} gtagCode The code for gtag
+ * @return {!JSX} This component
+ */
+export function CodeModal({popupId, ourCode, gtagCode}) {
+  const [showing, setShowing] = useState(false);
+  useEffect(()=> {
+    Prism.highlightAll();
+  });
+
+  const modal = <Modal show={showing} onHide={()=>setShowing(false)}>
+    <Tabs defaultActiveKey='internJunk' id={popupId}>
+      <Tab eventKey='internJunk' title='What we are making'>
+        <pre>
+          <code className='language-javascript'>
+            {ourCode}
+          </code>
+        </pre>
+      </Tab>
+      <Tab eventKey='gtag' title='gtag.js'>
+        <pre>
+          <code className='language-javascript'>
+            {gtagCode}
+          </code>
+        </pre>
+      </Tab>
+    </Tabs>
+  </Modal>;
+
+  return (<>
+    <Button onClick={()=>setShowing(true)}>Display Modal</Button>
+    {modal}
+  </>);
+}
