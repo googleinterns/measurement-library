491fae78f1dbda6e225b0561edad7af2076fd029
KJ Galvan <kaityagalvan@gmail.com>
2020-06-12 17:38:26 -0700

    Move home carousel, fix product links, tweak style

--- demo/src/screens/HomeScreen/HomeCarousel/HomeCarousel.js
+++ demo/src/screens/HomeScreen/HomeCarousel/HomeCarousel.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Carousel, Image} from 'react-bootstrap';
-import slideOne from '../../images/slide_one.png';
-import slideTwo from '../../images/slide_two.png';
-import slideThree from '../../images/slide_three.png';
+import {Link} from 'react-router-dom';
+import slideOne from '../../../images/slide_one.png';
+import slideTwo from '../../../images/slide_two.png';
+import slideThree from '../../../images/slide_three.png';
 import './HomeCarousel.css';
 
 /**
- * @return {!JSX} Slideshow component for showcasing
- *     our latest deals and offerings.
+ * Creates slideshow component for showcasing our latest deals and offerings.
+ * @return {!JSX} The component.
  */
 export function HomeCarousel() {
   return (
@@ -20,7 +21,9 @@ export function HomeCarousel() {
         />
         <Carousel.Caption>
           <h3>Stunning High Quality Prints Available</h3>
-          <p>View our <a href="/product/123">latest piece</a>.</p>
+          <p>{`View our `}
+            <Link to="/product/7ba94">latest piece</Link>.
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -31,7 +34,9 @@ export function HomeCarousel() {
         />
         <Carousel.Caption>
           <h3>SALE!</h3>
-          <p>Get this classic 50% off. <a href="/product/123">View Deal</a></p>
+          <p>{`Get this classic 50% off. `}
+            <Link to="/product/hjdf7">View Deal</Link>
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -42,7 +47,9 @@ export function HomeCarousel() {
         />
         <Carousel.Caption>
           <h3 className="font-italic">Toe Beans</h3>
-          <p>Seen like never before. <a href="/product/123">Buy Now</a></p>
+          <p>{`Seen like never before. `}
+            <Link to="/product/3h488">Buy Now</Link>
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
