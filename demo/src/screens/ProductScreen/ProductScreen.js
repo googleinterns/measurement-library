6a57ca6ae8dbd2c1006a2c7c75d20904b4d1f420
Pedro Silva <pedro.hs.silva0111@gmail.com>
2020-06-16 11:34:38 -0500

    functional product screen

--- demo/src/screens/ProductScreen/ProductScreen.js
+++ demo/src/screens/ProductScreen/ProductScreen.js
@@ -1,42 +1,63 @@
 import React from 'react';
 import {Container, Row, Col, Image, Button} from 'react-bootstrap';
-import blackDemo from '../../images/black.png';
+import {useParams} from 'react-router-dom';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+import {addOneToCart} from '../../store/StoreHelpers.js';
+
 
 /**
  * @return {!JSX} Page component for where a user can view
  *     details about a single product.
  */
-export function ProductScreen() {
+const ProductScreenBase = function({items, addToCart}) {
+  const {id} = useParams();
+
+  const currProduct = items[id];
+
+  const buttonText = currProduct.inCart ? 'In Cart' : 'Add to Cart';
+
+  const addOnClick = () => {
+    addToCart(id);
+  };
+
+  const buyOnClick = () => {
+    if (!currProduct.inCart) {
+      addToCart(id);
+    }
+  };
+
   return (
     <Container>
       <Row>
         <Col/>
         <Col xs={5}>
           <div>
-            <Image src={blackDemo} width="100%"/>
+            <Image src={currProduct.image} width="100%"/>
             <Container>
               <Row>
                 <Col>
-                    Name: Poe
+                    Name: {currProduct.name}
                 </Col>
                 <Col xs="8"/>
                 <Col>
-                    Price: $$$
+                    Price: ${currProduct.cost}
                 </Col>
               </Row>
             </Container>
             <br/>
             <p>
-              {`very good description of KJ's beatiful cat Poe.`}
+              {currProduct.description}
             </p>
             <Container>
               <Row>
                 <Col>
-                  <Button>Add to Cart</Button>
+                  <Button onClick={addOnClick} disabled = {currProduct.inCart}>{buttonText}</Button>
                 </Col>
                 <Col xs="3"/>
                 <Col>
-                  <Button variant="success">Buy Now</Button>
+                  <Button variant="success" onClick={buyOnClick} as={Link} to='/cart'>Buy Now</Button>
                 </Col>
               </Row>
             </Container>
@@ -46,4 +67,24 @@ export function ProductScreen() {
       </Row>
     </Container>
   );
-}
+};
+
+ProductScreenBase.propTypes = {
+  items: PropTypes.object,
+  addToCart: PropTypes.func,
+};
+
+const mapStateToProps = (state) => ({
+  items: state.items,
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addToCart: (id) => {
+      dispatch(addOneToCart(id));
+    },
+  };
+};
+
+export const ProductScreen = connect(mapStateToProps,
+    mapDispatchToProps)(ProductScreenBase);
