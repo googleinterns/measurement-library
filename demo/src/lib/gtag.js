1925be9d6b39a7ce464cae4c10609c2dc81f865d
KJ Galvan <kaityagalvan@gmail.com>
2020-06-17 10:42:11 -0700

    Change API for event function per peer request

--- demo/src/lib/gtag.js
+++ demo/src/lib/gtag.js
@@ -12,18 +12,23 @@ export const pageview = (pagePath) => {
 
 /**
  * Sends an event to Google Analytics.
+ * Can be spoofed to return mock code snippet of event instead.
  * View the [Developer Guide](https://developers.google.com/analytics/devguides/collection/app-web/events)
  * to review recommended events and their associated parameters.
  * @param {string} event
  * @param {!Object<string, *>} parameters
- * @return {string} Code snippet of the event being sent.
+ * @param {boolean=} spoof If true, returns mock code instead of sending event.
+ * @return {?string} Code snippet of the event being sent.
  */
-export const event = (event, parameters) => {
+export const event = (event, parameters, spoof = false) => {
+  if (spoof) {
+    return `gtag("event", "${event}", ` +
+    `${JSON.stringify(parameters, undefined, 2)})`;
+  }
+
   window.gtag('event', event, {
     send_to: GA_TRACKING_ID,
     ...parameters,
   });
-
-  return `gtag("event", "${event}", ` +
-      `${JSON.stringify(parameters, undefined, 2)})`;
+  return null;
 };
