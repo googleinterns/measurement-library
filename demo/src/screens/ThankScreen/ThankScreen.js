6e224d7b83f0b4217667a8cc679dd33a431383cd
Pedro Silva <pedro.hs.silva0111@gmail.com>
2020-06-18 14:40:29 -0500

    thanks screen completed

--- demo/src/screens/ThankScreen/ThankScreen.js
+++ demo/src/screens/ThankScreen/ThankScreen.js
@@ -5,20 +5,23 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
 /**
+ * @param {!Object} items The items stored in the site state
  * @return {!JSX} Page component where we thank the user
  *     for their purchase(s).
  */
 export const ThankScreenBase = function({items}) {
   const /** Array<!JSX> */ productListings = useMemo(() => {
+    const ITEMS_TO_DISPLAY = 6;
     const products = [];
     const usedItemSet = new Set();
 
     let i;
-    for (i = 0; i < 6; ++i) {
+    for (i = 0; i < ITEMS_TO_DISPLAY; ++i) {
       let maxPrice = 0;
       let maxPriceItem;
       let maxPriceID;
 
+      // Finds and pushes the most expensive item that has not yet been used.
       for (const [itemID, item] of Object.entries(items)) {
         if (item.cost > maxPrice && !usedItemSet.has(itemID)) {
           maxPriceItem = item;
@@ -69,8 +72,17 @@ ThankScreenBase.propTypes = {
   items: PropTypes.object,
 };
 
+/**
+  * @param {{items:Itemstore}} state
+  * @return {{items:Itemstore}} The items stored in the site state.
+  */
 const mapStateToProps = (state) => ({
   items: state.items,
 });
 
+/*
+ * Decorate the ProductScreenBase function, implicity passing in
+ * information about the global site state. Also pass mapDispatchToProps
+ * function to allow modification of global cart.
+ */
 export const ThankScreen = connect(mapStateToProps)(ThankScreenBase);
