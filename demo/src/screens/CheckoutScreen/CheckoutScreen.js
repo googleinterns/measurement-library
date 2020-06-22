c25822968b57323dbcb3e80d06b80fdd644e3752
KJ Galvan <kaityagalvan@gmail.com>
2020-06-22 11:31:56 -0700

    Clear cart after checkout

--- demo/src/screens/CheckoutScreen/CheckoutScreen.js
+++ demo/src/screens/CheckoutScreen/CheckoutScreen.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
 import {Container, Col, Row, Button} from 'react-bootstrap';
 import {UserInfoForm} from '../../components/UserInfoForm/UserInfoForm.js';
 import {useHistory} from 'react-router-dom';
@@ -6,6 +8,7 @@ import './CheckoutScreen.css';
 import {MiniCart} from '../../components/MiniCart/MiniCart.js';
 // eslint-disable-next-line max-len
 import {BillingInfoForm} from '../../components/BillingInfoForm/BillingInfoForm.js';
+import {clearCart} from '../../store/StoreHelpers.js';
 
 /**
  * The ID for the personal info form the user will fill out on this page.
@@ -22,9 +25,10 @@ const BILLING_FORM_ID = 'billing-info-form';
 /**
  * Page component for a user to enter in personal billing information
  * and confirm the items in their cart.
+ * @param {function()} clearCart A function to clear all items from cart.
  * @return {!JSX}
  */
-export function CheckoutScreen() {
+const CheckoutScreenBase = function({clearCart}) {
   const [shippingDone, setShippingDone] = useState(false);
   const /** !Object */ history = useHistory();
 
@@ -51,6 +55,7 @@ export function CheckoutScreen() {
     const formPersonal = document.getElementById(USER_FORM_ID);
     const formBilling = document.getElementById(BILLING_FORM_ID);
     if (formBilling.checkValidity() && formPersonal.checkValidity()) {
+      clearCart();
       // navigate to thank you page with react-router
       history.push('/thanks');
     } else {
@@ -86,4 +91,11 @@ export function CheckoutScreen() {
       </Row>
     </Container>
   );
-}
+};
+
+CheckoutScreenBase.propTypes = {
+  clearCart: PropTypes.func,
+};
+
+export const CheckoutScreen = connect(null,
+    {clearCart})(CheckoutScreenBase);
