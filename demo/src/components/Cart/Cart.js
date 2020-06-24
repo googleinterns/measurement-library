55e27b38345f8b660f5a4174ba518ac9358e1ed9
Alex Li <7alex7li@gmail.com>
2020-06-24 09:25:16 -0400

    Fixed indentation and switch to arrow functions

--- demo/src/components/Cart/Cart.js
+++ demo/src/components/Cart/Cart.js
@@ -15,13 +15,13 @@ import {sendRemoveFromCartEvent} from '../../lib/gtagEvents';
  * Creates a component describing a shopping Cart.
  * @param {ItemStore} items The global {@link ItemStore} site object.
  * @param {function(string, number)} setQuantity A function to modify
- *      the quantity of an item in the global state.
+ *     the quantity of an item in the global state.
  * @param {function(string)} removeOneFromCart A function to reduce the
- * quantity of an element in the cart by 1, deleting it if the quantity is
- * empty.
+ *     quantity of an element in the cart by 1, deleting it if the quantity is
+ *     empty.
  * @return {!JSX} The component.
  */
-const CartBase = function({items, setQuantity, removeOneFromCart}) {
+const CartBase = ({items, setQuantity, removeOneFromCart}) => {
   const /** Array<!JSX> */ itemsRender = [];
 
   // Create the content of the cart display, with one row per item.
