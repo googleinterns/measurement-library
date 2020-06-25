3b6fdfa69c19b118e574dc56be34e8dca8048497
pedrosilva0111 <48192017+pedrosilva0111@users.noreply.github.com>
2020-06-25 11:20:34 -0500

    Update ThankScreen.js

--- demo/src/screens/ThankScreen/ThankScreen.js
+++ demo/src/screens/ThankScreen/ThankScreen.js
@@ -5,7 +5,7 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
 /**
- * @param {{items:Array}} items The items stored in the site state
+ * @param {{items:Array}} items The items stored in the site state.
  * @return {!JSX} Page component where we thank the user
  *     for their purchase(s).
  */
