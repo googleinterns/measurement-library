875d43a03df65a9f5eda723ddc7d755087575efd
Alex Li <7alex7li@gmail.com>
2020-08-06 10:40:01 -0400

    Add code to respond to the event function

--- src/setup.js
+++ src/setup.js
@@ -27,9 +27,33 @@ function setupMeasure(dataLayer) {
    * The DataLayerHelper to use with this application.
    * @private @const {!DataLayerHelper}
    */
-  const helper = new DataLayerHelper(dataLayer);
+  const helper = new DataLayerHelper(dataLayer, {'processNow': false});
 
-   // TODO: Use the variables declared above
+  helper.registerProcessor('config', configProcessors);
+  helper.process();
+
+
+  // TODO: configProcessors jsdoc and better processor/storage init in PR 52
+  function configProcessors(eventProcessor, eventOptions,
+                            storageInterface, storageOptions) {
+    let processor = new eventProcessor(eventOptions);
+    let storage = new storageInterface(storageOptions);
+
+    /**
+     * Process an event object by sending it to the registered event processor
+     * along with interfaces to the storage and data layer helper.
+     *
+     * @param {string} name The name of the event to process.
+     * @param {!Object<string, *>=} args The arguments to
+     *     process with the event.
+     */
+    function processEvent(name, args = undefined) {
+      if (!args) args = {};
+      processor.processEvent(storage, this, name, args);
+    }
+
+    helper.registerProcessor('event', processEvent);
+  }
 }
 
 goog.exportSymbol('setupMeasure', setupMeasure);
--- test/integration/scope_test.js
+++
@@ -1,9 +0,0 @@
-describe('Test that the integration tests are running', () => {
-  it(`creates a global setupMeasure function`, () => {
-    expect(window.setupMeasure).toBeInstanceOf(Function);
-  });
-
-  it(`creates a global dataLayer array`, () => {
-    expect(window.dataLayer).toBeInstanceOf(Array);
-  });
-});
