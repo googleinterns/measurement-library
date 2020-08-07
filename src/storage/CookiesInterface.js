53a2a3c7dcf3e07da6f18e96fc163937f8701c93
Pedro Silva <pedro.hs.silva0111@gmail.com>
2020-08-07 11:22:02 -0500

    finished settings configuration for cookiesStorage and testing for registrable domain

---
+++ src/storage/CookiesInterface.js
@@ -0,0 +1,80 @@
+goog.module('measurementLibrary.storage.CookiesStorage');
+
+/**
+ * A fancier storage that does cool things.
+ * @implements {StorageInterface}
+ */
+class CookiesStorage {
+  /** @param {!Object.<string, string|number|boolean>} settings an object
+   *      holding the settings to be set on this instance of CookiesStorage.
+   */
+  constructor(settings) {
+    /** @private @const {!Object.<string, string|number|boolean>}*/
+    this.settings_ = settings;
+
+    this.setDefaults_();
+  }
+
+  /**
+   * Helps the constructor set the default for any omitted settings.
+   * @private
+   */
+  setDefaults_() {
+    if (!this.settings_['domain']) {
+      this.settings_['domain'] = this.findAutoDomain_(document);
+    }
+    if (!this.settings_['expires']) {
+      // 28 days in seconds, default expiry time.
+      const defaultTime = 28 * 24 * 60 * 60;
+      this.settings_['expires'] = defaultTime;
+    }
+    if (!this.settings_['prefix']) {
+      // ml for measurement library. should be discussed.
+      const defaultPrefix = 'ml_';
+      this.settings_['prefix'] = defaultPrefix;
+    }
+    if (!this.settings_['update']) {
+      const defaultUpdate = true;
+      this.settings_['update'] = defaultUpdate;
+    }
+    if (!this.settings_['flags']) {
+      const defaultFlags = '';
+      this.settings_['flags'] = defaultFlags;
+    }
+  }
+
+  /**
+   * Finds the registrable domain of current URL and sets it as the default
+   *    domain for cookies
+   * @param {!document} document the document whose cookies are to be set.
+   * @return {?string} the registrable domain of docDomain
+   * @private
+   */
+  findAutoDomain_(document) {
+    const oldCookies = document.cookie;
+    const domainArray = document.domain.split('.');
+    let domain = domainArray.pop();
+    const newCookie = `registrable_domain=${domain};domain=${domain}`;
+    document.cookie = newCookie;
+
+    while (document.cookie === oldCookies && domainArray.length > 0) {
+      domain = `${domainArray.pop() }.${ domain }`;
+      const newCookie = `registrable_domain=${ domain };domain=${ domain }`;
+      document.cookie = newCookie;
+    }
+
+    if (document.cookie === oldCookies) {
+      return null;
+    }
+
+    return domain;
+  }
+
+  /** @override */
+  save(key, value, secondsToLive) {}
+
+  /** @override */
+  load(key, defaultValue) {}
+}
+
+exports = CookiesStorage;
---
+++ test/findAutoDomain_test.js
@@ -0,0 +1,53 @@
+goog.module('measurementLibrary.storage.CookiesStorage.testing.findAutoDomain_');
+goog.setTestOnly();
+
+const CookiesStorage = goog.require('measurementLibrary.storage.CookiesStorage');
+
+describe('the findAutoDomain method', () => {
+  let testDocument;
+  let setupDocument;
+  const storage = new CookiesStorage({});
+
+  beforeEach(() => {
+    // sets up testDocument, an object that acts like the window.document object
+    // when dealing with cookies with a specified domain. Allows for testing on
+    // multiple domains.
+    setupDocument = (domain, registrableDomain) => {
+      testDocument = {'cook': ''};
+      testDocument['domain'] = domain;
+      Object.defineProperty(testDocument, 'cookie', {
+        get() {
+          return testDocument.cook;
+        },
+        set(newValue) {
+          if (newValue.includes(`domain=${registrableDomain}`)) {
+            testDocument.cook = newValue;
+          }
+        },
+        configurable: true,
+      });
+    };
+  });
+
+  it('returns null when domain is a public suffix', () => {
+    setupDocument('com');
+
+    expect(storage.findAutoDomain_(testDocument)).toBe(null);
+  });
+
+  it('returns null when domain does not contain a public suffix', () => {
+    setupDocument('[2001:0db8:85a3:0000:0000:8a2e:0370:7334]');
+
+    expect(storage.findAutoDomain_(testDocument)).toBe(null);
+  });
+
+  it('returns registrableDomain when domain is a valid subdomain', () => {
+    setupDocument('subdomain.example.com', 'example.com');
+
+    expect(storage.findAutoDomain_(testDocument)).toBe('example.com');
+
+    setupDocument('www.subdomain.example.com', 'example.com');
+
+    expect(storage.findAutoDomain_(testDocument)).toBe('example.com');
+  });
+});
