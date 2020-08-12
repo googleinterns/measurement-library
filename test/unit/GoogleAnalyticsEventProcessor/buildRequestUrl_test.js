56121d15db8a0ce105f29c0625a16723e55b0b02
KJ Galvan <kaityagalvan@gmail.com>
2020-08-11 18:25:52 -0700

    update test

--- test/unit/GoogleAnalyticsEventProcessor/buildRequestUrl_test.js
+++ test/unit/GoogleAnalyticsEventProcessor/buildRequestUrl_test.js
@@ -3,16 +3,16 @@ goog.setTestOnly();
 
 const GoogleAnalyticsEventProcessor = goog.require('measurementLibrary.eventProcessor.GoogleAnalyticsEventProcessor');
 
-describe('The `buildRequestUrl` method ' +
+describe('The `buildRequestUrl_` method ' +
     'of GoogleAnalyticsEventProcessor', () => {
   /**
-   * Tests the `buildRequestUrl` function by passing in an API secret,
-   * measurement ID, and measurement URL to construct the event processor
-   * with and comparing the result of the function with the expected result.
-   * @param {string} apiSecret
-   * @param {string} measurementId
-   * @param {string} measurementUrl
-   * @return {string} expected
+   * Builds expectation object for the `buildRequestUrl_` function by passing
+   * in an API secret, measurement ID, and measurement URL to construct the
+   * event processor with for the function call.
+   * @param {string|undefined} apiSecret
+   * @param {string|undefined} measurementId
+   * @param {string|undefined} measurementUrl
+   * @return {!Expectation}
    */
   function expectRequestUrl(apiSecret, measurementId, measurementUrl) {
     const eventProcessor = new GoogleAnalyticsEventProcessor({
@@ -29,27 +29,27 @@ describe('The `buildRequestUrl` method ' +
     expectRequestUrl(
       /* apiSecret */ '123',
       /* measurementId */ '456',
-      /* measurementUrl */ undefined)
-        .toBe('https://www.google-analytics.com/mp/collect?' +
-            'measurement_id=456&api_secret=123');
+      /* measurementUrl */ undefined
+    ).toBe('https://www.google-analytics.com/mp/collect?' +
+        'measurement_id=456&api_secret=123');
 
     expectRequestUrl(
-      /* apiSecret */ '',
+      /* apiSecret */ undefined,
       /* measurementId */ '456',
-      /* measurementUrl */ undefined)
-        .toBe('https://www.google-analytics.com/mp/collect?measurement_id=456');
+      /* measurementUrl */ undefined
+    ).toBe('https://www.google-analytics.com/mp/collect?measurement_id=456');
 
     expectRequestUrl(
       /* apiSecret */ '123',
-      /* measurementId */ '',
-      /* measurementUrl */ undefined)
-        .toBe('https://www.google-analytics.com/mp/collect?api_secret=123');
+      /* measurementId */ undefined,
+      /* measurementUrl */ undefined
+    ).toBe('https://www.google-analytics.com/mp/collect?api_secret=123');
 
     expectRequestUrl(
-      /* apiSecret */ '',
-      /* measurementId */ '',
-      /* measurementUrl */ undefined)
-        .toBe('https://www.google-analytics.com/mp/collect');
+      /* apiSecret */ undefined,
+      /* measurementId */ undefined,
+      /* measurementUrl */ undefined
+    ).toBe('https://www.google-analytics.com/mp/collect');
   });
 
   it('creates the correct query string when using an overriden ' +
@@ -57,33 +57,53 @@ describe('The `buildRequestUrl` method ' +
     expectRequestUrl(
       /* apiSecret */ '123',
       /* measurementId */ '456',
-      /* measurementUrl */ 'https://test.com')
-        .toBe('https://test.com?measurement_id=456&api_secret=123');
+      /* measurementUrl */ 'https://test.com'
+    ).toBe('https://test.com?measurement_id=456&api_secret=123');
 
     expectRequestUrl(
-      /* apiSecret */ '',
+      /* apiSecret */ undefined,
       /* measurementId */ '456',
-      /* measurementUrl */ 'https://test.com')
-        .toBe('https://test.com?measurement_id=456');
+      /* measurementUrl */ 'https://test.com'
+    ).toBe('https://test.com?measurement_id=456');
 
     expectRequestUrl(
       /* apiSecret */ '123',
-      /* measurementId */ '',
-      /* measurementUrl */ 'https://test.com')
-        .toBe('https://test.com?api_secret=123');
+      /* measurementId */ undefined,
+      /* measurementUrl */ 'https://test.com'
+    ).toBe('https://test.com?api_secret=123');
 
     expectRequestUrl(
-      /* apiSecret */ '',
-      /* measurementId */ '',
-      /* measurementUrl */ 'https://test.com')
-        .toBe('https://test.com');
+      /* apiSecret */ undefined,
+      /* measurementId */ undefined,
+      /* measurementUrl */ 'https://test.com'
+    ).toBe('https://test.com');
   });
 
   it('encodes query parameters', () => {
     expectRequestUrl(
       /* apiSecret */ '12 3',
       /* measurementId */ '45 6',
-      /* measurementUrl */ 'https://test.com')
-        .toBe('https://test.com?measurement_id=45%206&api_secret=12%203');
+      /* measurementUrl */ 'https://test.com'
+    ).toBe('https://test.com?measurement_id=45%206&api_secret=12%203');
+  });
+
+  it('does not add empty strings as query parameters', () => {
+    expectRequestUrl(
+      /* apiSecret */ '',
+      /* measurementId */ undefined,
+      /* measurementUrl */ 'https://test.com'
+    ).toBe('https://test.com');
+
+    expectRequestUrl(
+      /* apiSecret */ undefined,
+      /* measurementId */ '',
+      /* measurementUrl */ 'https://test.com'
+    ).toBe('https://test.com');
+
+    expectRequestUrl(
+      /* apiSecret */ '',
+      /* measurementId */ '',
+      /* measurementUrl */ 'https://test.com'
+    ).toBe('https://test.com');
   });
 });
