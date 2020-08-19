691f74c745e528d18b8276281d2338e26366ddc6
Alex Li <7alex7li@gmail.com>
2020-08-19 14:40:09 -0400

    Move merge and getExtraOptions to top level, they will be helpful later

--- src/config/configProcessors.js
+++ src/config/configProcessors.js
@@ -84,6 +84,45 @@ function buildProcessor_(constructor, params) {
 }
 
 /**
+ *  Perform a merge on the given list of objects. Any properties that both
+ *  the ith and jth (i less than j) objects both share will be overwritten
+ *  to have just the property of the jth object.
+ *
+ *  @param {...} args The objects to merge.
+ *  @return {!Object<string, *>}
+ */
+const merge = function(args) {
+  const result = {};
+  for (let i = 0; i < arguments.length; i++) {
+    const toMerge = arguments[i];
+    for (const prop in toMerge) {
+      if (toMerge.hasOwnProperty(prop)) {
+        if (toMerge[prop] === undefined) {
+          delete result[prop];
+        } else {
+          result[prop] = toMerge[prop];
+        }
+      }
+    }
+  }
+  return result;
+};
+
+/**
+ *  Get extra parameters that are stored in the data layer for a given object.
+ *
+ *  @param {?DataLayerHelper} helper
+ *  @param {?Object} object The object that we would like to get parameters of.
+ *  @return {!Object<string, *>}
+ */
+const getExtraOptions = (helper, object) => {
+  if (helper && object && object.hasOwnProperty('getName')) {
+    return /** @type {!Object<string, *>} */ (helper.get(object.getName()));
+  }
+  return {};
+};
+
+/**
  * When called with an implementation of the eventProcessor and
  * storageInterface API, this function registers processors to react to the
  * 'set' and 'event' commands.
@@ -113,37 +152,9 @@ function configProcessors(helper, eventProcessor, eventOptions,
   // Process an event object by sending it to the registered event processor
   // along with interfaces to the storage and data layer helper.
   function processEvent(name, options = undefined) {
-    /**
-     *  Perform a merge on the given list of objects. Any properties that both
-     *  the ith and jth (i less than j) objects both share will be overwritten
-     *  to have just the property of the jth object.
-     *  This jsdoc is needed so the compiler doesn't warn
-     *
-     *  @param {...} args The objects to merge.
-     *  @return {!Object<string, *>}
-     */
-    const merge = function(args) {
-      const result = {};
-      for (let i = 0; i < arguments.length; i++) {
-        const toMerge = arguments[i];
-        for (const prop in toMerge) {
-          if (toMerge.hasOwnProperty(prop)) {
-            result[prop] = toMerge[prop];
-          }
-        }
-      }
-      return result;
-    };
-
-    const getExtraOptions = (object) => {
-      if (object && object.hasOwnProperty('getName')) {
-        return helper.get(object.getName());
-      }
-      return {};
-    };
     const model = this;
     if (!options) options = {};
-    options = merge(getExtraOptions(processor), eventOptions, options);
+    options = merge(getExtraOptions(helper, processor), eventOptions, options);
     processor.processEvent(/** @type {!StorageInterface} */(storage),
       model, name, options);
   }
