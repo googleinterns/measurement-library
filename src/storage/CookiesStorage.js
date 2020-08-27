a312b15290f3b6797d207e0381a1b483e28f8f20
Pedro Silva <pedro.hs.silva0111@gmail.com>
2020-08-27 12:46:22 -0500

    Address cookie edge cases

--- src/storage/CookiesStorage.js
+++ src/storage/CookiesStorage.js
@@ -137,11 +137,13 @@ class CookiesStorage {
     // cookie structure is key=val;key=val OR key=val; key=val. This boolean
     // statement takes care of both possibilities.
     const resultCookie = cookieArray.find((row) =>
-      row.indexOf(`${this.settings_['prefix']}${key}`) >= 0
+      row.indexOf(`${this.settings_['prefix']}${key}=`) === 0
     );
     const resultValArray = resultCookie.split('=');
 
     let resultVal = resultValArray[1];
+
+    // There could be '=' in the cookie value.
     if (resultValArray.length > 2) {
       for (let i = 2; i < resultValArray.length; i++) {
         resultVal += '=';
